4a09baa4893e7549cbd252b9743bcf048731e131
Melih Yayli <melih.yayli@sabanciuniv.edu>
2021-09-22 16:56:55 +0300

    Tweaks
    Tweaks

--- spring-boot-security-jwt-auth-mongodb/src/main/java/com/channelpizza/webapp/SpringBootSecurityJwtMongodbApplication.java
+++ spring-boot-security-jwt-auth-mongodb/src/main/java/com/channelpizza/webapp/SpringBootSecurityJwtMongodbApplication.java
@@ -10,6 +10,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -22,55 +23,67 @@ public class SpringBootSecurityJwtMongodbApplication {
 	@Bean
 	CommandLineRunner commandLineRunner(PizzaRepository pizzaRepository, MongoTemplate mongoTemplate) {
 		return args -> {
-			Pizza pizza1 = new Pizza(
-					"Margherita",2.5,"tomatoes, mozzarella, basil, olive oil","http://simply-delicious-food.com/wp-content/uploads/2020/06/Grilled-Pizza-Margherita-3.jpg"
-			);
-			Pizza pizza2 = new Pizza(
-					"Pepperoni",2.75,"pepperoni, mozzarela, oregano","https://www.simplyrecipes.com/thmb/HQJVIRwkySb-QiYv6saC9dEjU5A=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-4-82c60893fcad4ade906a8a9f59b8da9d.jpg"
-			);
-			Pizza pizza3 = new Pizza(
-					"Calzone",3.99,"ricotta, mozzarella, salami, parmesan","https://www.happyfoodstube.com/wp-content/uploads/2016/03/calzone-pizza-picture.jpg"
-			);
-			Pizza pizza4 = new Pizza(
-					"Quattro Formaggi",5.05,"mozzarella, gorgonzola, ricotta, parmesan","https://image.freepik.com/free-photo/delicious-italian-pizza-quattro-formaggi-with-four-cheeses_194143-1059.jpg"
-			);
 
-			String pName1 = pizza1.getPizzaName();
-			String pName2 = pizza2.getPizzaName();
-			String pName3 = pizza3.getPizzaName();
-			String pName4 = pizza4.getPizzaName();
+			List<Pizza> pizzasToBeCreated = new ArrayList<>();
 
-			if(pizzaRepository.findPizzaByPizzaName(pName1) != null) {
-				System.out.println("Pizza already exists");
-			} else {
-				pizzaRepository.insert(pizza1);
-				System.out.println("Pizza " + pName1 + " has been created.");
+			pizzasToBeCreated.add(new Pizza(
+					"Margherita",
+					2.5,
+					"tomatoes, mozzarella, basil, olive oil",
+					"http://simply-delicious-food.com/wp-content/uploads/2020/06/Grilled-Pizza-Margherita-3.jpg"
+			));
+			pizzasToBeCreated.add(new Pizza(
+					"Pepperoni",
+					2.75,
+					"pepperoni, mozzarela, oregano",
+					"https://www.simplyrecipes.com/thmb/HQJVIRwkySb-QiYv6saC9dEjU5A=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-4-82c60893fcad4ade906a8a9f59b8da9d.jpg"
+			));
+			pizzasToBeCreated.add(new Pizza(
+					"Calzone",
+					3.99,
+					"ricotta, mozzarella, salami, parmesan",
+					"https://www.happyfoodstube.com/wp-content/uploads/2016/03/calzone-pizza-picture.jpg"
+			));
+			pizzasToBeCreated.add(new Pizza(
+					"Quattro Formaggi",
+					5.05,
+					"mozzarella, gorgonzola, ricotta, parmesan",
+					"https://image.freepik.com/free-photo/delicious-italian-pizza-quattro-formaggi-with-four-cheeses_194143-1059.jpg"
+			));
+			pizzasToBeCreated.add(new Pizza(
+					"Mushroom",
+					5.05,
+					"Garlic, Mozzarella, Mushrooms, Arugula, Basil",
+					"https://www.theroastedroot.net/wp-content/uploads/2013/08/Chicken-Mushroom-Pesto-Pizza.jpg"
+			));
+			pizzasToBeCreated.add(new Pizza(
+					"Hawaiian",
+					5.05,
+					"ham, pineapples, cheese",
+					"https://sparkpeo.hs.llnwd.net/e1/resize/630m620/e2/guid/Hawaiian-Pizza-SparkRecipes-Un-Chained-Contest-Finalist-/788bdd98-898c-4900-ad38-9fa6ad1fe0dd.jpg"
+			));
+			pizzasToBeCreated.add(new Pizza(
+					"Buffalo",
+					5.05,
+					"shredded chicken, buffalo sauce, blue cheese crumbles, cilantro",
+					"https://assets.website-files.com/5f1651e7d193211bb34def1a/5f1f08dcbf513648fb367834_pizza_t20_KoN9gV.jpg"
+			));
+			pizzasToBeCreated.add(new Pizza(
+					"BBQ Chicken",
+					5.05,
+					"bbq sauce, mozzarella, chicken, red onions, cilantro",
+					"https://www.diabetesfoodhub.org/system/thumbs/system/images/recipes/Recid_76_BBQ_Chicken_Pizza_iStock-484653761_02262018_2959761669.jpg"
+			));
+
+			for (int i = 0; i < pizzasToBeCreated.size(); i++) {
+				String tempPizzaName = pizzasToBeCreated.get(i).getPizzaName();
+				if(pizzaRepository.findPizzaByPizzaName(tempPizzaName) != null) {
+					System.out.println("Pizza already exists");
+				} else {
+					pizzaRepository.insert(pizzasToBeCreated.get(i));
+					System.out.println("Pizza " + tempPizzaName + " has been created.");
+				}
 			}
-
-			if(pizzaRepository.findPizzaByPizzaName(pName2) != null) {
-				System.out.println("Pizza already exists");
-			} else {
-				pizzaRepository.insert(pizza2);
-				System.out.println("Pizza " + pName2 + " has been created.");
-			}
-
-			if(pizzaRepository.findPizzaByPizzaName(pName3) != null) {
-				System.out.println("Pizza already exists");
-			} else {
-				pizzaRepository.insert(pizza3);
-				System.out.println("Pizza " + pName3 + " has been created.");
-			}
-
-			if(pizzaRepository.findPizzaByPizzaName(pName4) != null) {
-				System.out.println("Pizza already exists");
-			} else {
-				pizzaRepository.insert(pizza4);
-				System.out.println("Pizza " + pName4 + " has been created.");
-			}
-
-
-
 		};
 	}
-
 }
